658b09f59f1bd0bffb2c05ca206ecc3527692e2a
selimdoyranli <selimdoyranli@gmail.com>
2025-11-24 22:33:25 +0300

    refactor: memoize hud & skills component for fix re-render

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,49 +1,51 @@
+import { memo } from 'react';
 import styles from './Skills.module.css';
 import { Button } from '@mantine/core';
 import { Icon } from '@iconify/react';
 import { SkillType } from '../common/constants';
 
-export default function Skills() {
-  const skills = [
-    {
-      type: SkillType.TELEPORT,
-      icon: 'fluent-emoji:cyclone',
-      key: 'Q'
-    },
-    {
-      type: SkillType.HOMING_MISSILE,
-      icon: 'fluent-emoji:comet',
-      key: 'W'
-    },
-    {
-      type: SkillType.LASER_BEAM,
-      icon: 'fluent-emoji:water-pistol',
-      key: 'E'
-    },
-    {
-      type: SkillType.INVINCIBILITY,
-      icon: 'fluent-emoji:shield',
-      key: 'R'
-    }
-  ];
-  const getCooldownId = (skillType: SkillType) => {
-    switch (skillType) {
-      case SkillType.TELEPORT:
-        return 'cd-missile';
-      case SkillType.HOMING_MISSILE:
-        return 'cd-basic';
-      case SkillType.LASER_BEAM:
-        return 'cd-slash';
-      case SkillType.INVINCIBILITY:
-        return 'cd-tank';
-      default:
-        return '';
-    }
-  };
+const SKILLS = [
+  {
+    type: SkillType.TELEPORT,
+    icon: 'fluent-emoji:cyclone',
+    key: 'Q'
+  },
+  {
+    type: SkillType.HOMING_MISSILE,
+    icon: 'fluent-emoji:comet',
+    key: 'W'
+  },
+  {
+    type: SkillType.LASER_BEAM,
+    icon: 'fluent-emoji:water-pistol',
+    key: 'E'
+  },
+  {
+    type: SkillType.INVINCIBILITY,
+    icon: 'fluent-emoji:shield',
+    key: 'R'
+  }
+] as const;
 
+const getCooldownId = (skillType: SkillType): string => {
+  switch (skillType) {
+    case SkillType.TELEPORT:
+      return 'cd-missile';
+    case SkillType.HOMING_MISSILE:
+      return 'cd-basic';
+    case SkillType.LASER_BEAM:
+      return 'cd-slash';
+    case SkillType.INVINCIBILITY:
+      return 'cd-tank';
+    default:
+      return '';
+  }
+};
+
+function Skills() {
   return (
     <div id="skills" className={styles.skills}>
-      {skills.map((skill) => (
+      {SKILLS.map((skill) => (
         <Button
           key={skill.type}
           className={styles.skillSlot}
@@ -63,4 +65,6 @@ export default function Skills() {
     </div>
   );
 }
+
+export default memo(Skills);
 
