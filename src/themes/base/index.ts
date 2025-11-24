b460f177f1e58c74c2d4cdd9842172575511fc71
selimdoyranli <selimdoyranli@gmail.com>
2025-11-24 19:50:43 +0300

    feat: ui improvements

--- src/themes/base/index.ts
+++ src/themes/base/index.ts
@@ -1,11 +1,16 @@
 import { createTheme } from '@mantine/core';
 
 export const theme = createTheme({
+  fontFamily: 'Kode Mono, monospace',
+  headings: {
+    fontFamily: 'Kode Mono, monospace'
+  },
+  primaryColor: 'cyan',
   radius: {
     xs: '0px',
     sm: '0px',
     md: '0px',
     lg: '0px',
     xl: '0px',
-  }
+  },
 });
